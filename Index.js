ac1b1ec63d0898c56a9c1b000601b7c257978fca
Djunio Filho <djunio_rmf@hotmail.com>
2023-04-24 19:51:48 -0300

    start

---
+++ Index.js
@@ -0,0 +1,105 @@
+const express = require('express');
+const fileUpload = require('express-fileupload');
+const DCMFile = require('dcmjs').data.DCMFile;
+const hl7 = require('hl7');
+const app = express();
+
+const fs = require('fs');
+const rateLimit = require('express-rate-limit');
+
+const limiter = rateLimit({
+    windowMs: 60 * 1000, // 1 minuto
+    max: 300, // Limite de 3 requisições por minuto
+});
+
+app.use(express.json());
+app.use(fileUpload());
+
+app.post('/anonimize-dicom', limiter, async (req, res) => {
+    try {
+        // Verifica se o arquivo DICOM foi enviado
+        if (!req.files || !req.files.file) {
+            return res.status(400).send('Arquivo DICOM não enviado');
+        }
+
+        // Lê o arquivo DICOM enviado
+        const file = req.files.file.data;
+        const dcm = new DCMFile(file);
+
+        // Anonimiza o nome do paciente
+        const name = dcm.getElement('0010', '0010');
+        const nameValue = name.value.slice(0, 3) + '*'.repeat(name.value.length - 6) + name.value.slice(-3);
+        name.value = nameValue;
+
+        // Anonimiza a data de nascimento do paciente
+        const dob = dcm.getElement('0010', '0030');
+        const dobValue = dob.value.slice(0, 6) + '*'.repeat(dob.value.length - 6);
+        dob.value = dobValue;
+
+        // Anonimiza o número de identificação do paciente
+        const id = dcm.getElement('0010', '0020');
+        const idValue = '*'.repeat(id.value.length - 4) + id.value.slice(-4);
+        id.value = idValue;
+
+        // Anonimiza o nome da mãe do paciente
+        const mother = dcm.getElement('0010', '1060');
+        mother.value = '*'.repeat(mother.value.length);
+
+        // Anonimiza o endereço do paciente
+        const address = dcm.getElement('0038', '0400');
+        address.value = '*'.repeat(address.value.length);
+
+        // Anonimiza o telefone do paciente
+        const phone = dcm.getElement('0010', '2154');
+        phone.value = '*'.repeat(phone.value.length);
+
+        // Retorna o arquivo DICOM anonimizado
+        const anonFile = dcm.write();
+        res.setHeader('Content-disposition', 'attachment; filename=anon.dcm');
+        res.setHeader('Content-type', 'application/dicom');
+        res.send(anonFile);
+    } catch (err) {
+        console.error(err);
+        res.status(500).send('Ocorreu um erro durante a anonimização do arquivo DICOM');
+    }
+});
+
+// Endpoint POST para anonimizar um arquivo HL7
+app.post('/anonimizar-hl7', limiter, async (req, res) => {
+
+    try {
+        // Verificar se foi enviado um arquivo HL7 na requisição
+        if (!req.body.arquivoHL7) {
+            return res.status(400).send({ mensagem: 'Arquivo HL7 não encontrado na requisição.' });
+        }
+
+        // Ler o conteúdo do arquivo HL7 enviado na requisição
+        const conteudoHL7 = req.body.arquivoHL7;
+
+        // Converter o conteúdo para objeto HL7
+        const parser = new hl7.Parser();
+        const mensagemHL7 = parser.parse(conteudoHL7);
+
+        // Anonimizar os dados do paciente na mensagem HL7
+        const nomePaciente = mensagemHL7['PID']['5']['1'];
+        mensagemHL7['PID']['5']['1'] = `${nomePaciente.substring(0, 3)}${'*'.repeat(nomePaciente.length - 6)}${nomePaciente.substring(nomePaciente.length - 3, nomePaciente.length)}`;
+        mensagemHL7['PID']['7'] = '';
+        mensagemHL7['PID']['8'][0] = '';
+        mensagemHL7['PID']['10'] = '';
+        mensagemHL7['PID']['11'] = '';
+        mensagemHL7['PID']['19'][0] = '';
+        mensagemHL7['PID']['22'] = '';
+
+        // Converter a mensagem HL7 anonimizada para string
+        const serializer = new hl7.Serializator();
+        const mensagemAnonimizada = serializer.serialize(mensagemHL7);
+
+        // Retornar o arquivo HL7 anonimizado como resposta
+        res.setHeader('Content-Disposition', 'attachment; filename=arquivo-anonimizado.hl7');
+        res.setHeader('Content-Type', 'application/octet-stream');
+        res.send(mensagemAnonimizada);
+    } catch (err) {
+        console.error(err);
+        res.status(500).send('Ocorreu um erro durante a anonimização do arquivo hl7');
+    }
+});
--- README.md
+++ README.md
@@ -1,2 +1,61 @@
-# AnonMedIm
- Anonymization of Medical Image files
+# "AnonMedIm" - uma abreviação criativa para "Anonymization of Medical Image files".
+**Uma API de Anonimização de Dados de Paciente em Arquivos HL7 e DICOM**
+Esta API tem como objetivo receber arquivos no formato HL7 ou DICOM contendo informações de pacientes e realizar a anonimização desses dados, respeitando as diretrizes da Lei Geral de Proteção de Dados (LGPD).
+
+## A API possui dois métodos principais:
+
+- POST /anonimizar-hl7: Recebe um arquivo HL7 no corpo da requisição, realiza a anonimização dos dados do paciente e retorna o arquivo anonimizado para download.
+- POST /anonimizar-dicom: Recebe um arquivo DICOM no corpo da requisição, realiza a anonimização dos dados do paciente e retorna o arquivo anonimizado para download.
+
+Ambos os métodos seguem o mesmo fluxo: primeiro, é verificado se o arquivo foi enviado corretamente na requisição. Em seguida, o arquivo é lido e convertido para um objeto HL7 ou DICOM, dependendo do método utilizado. Os dados do paciente são então anonimizados, seguindo as diretrizes estabelecidas. Por fim, o arquivo anonimizado é retornado como resposta para download.
+Além disso, a API também possui um limite de 3 requisições por minuto para evitar sobrecarga do servidor.
+
+Para utilizar a API, basta fazer uma requisição POST para a URL correspondente ao método desejado, enviando o arquivo a ser anonimizado no corpo da requisição. Caso a requisição seja bem-sucedida, o arquivo anonimizado será retornado como resposta, pronto para download.
+
+**Exemplo de requisição utilizando o método /anonimizar-hl7:**
+
+```sh
+POST http://localhost:3000/anonimizar-hl7
+
+Corpo da requisição:
+{
+    "arquivoHL7": "conteúdo do arquivo HL7"
+}
+```
+**Resposta:**
+O arquivo HL7 anonimizado será retornado como download.
+
+**Exemplo de requisição utilizando o método /anonimizar-dicom:**
+```sh
+POST http://localhost:3000/anonimizar-dicom
+
+Corpo da requisição:
+{
+    "arquivoDICOM": "conteúdo do arquivo DICOM"
+}
+```
+
+**Resposta:**
+O arquivo DICOM anonimizado será retornado como download.
+Lembre-se de respeitar o limite de 300 requisições por minuto para evitar bloqueios ou sobrecarga do servidor.
+
+## Contribuidores
+Este projeto foi desenvolvido por:
+- Djunio Rosa de Melo FIlho
+
+# General Public License:
+
+The code is under the [GNU GPL v3](https://www.gnu.org/licenses/gpl-3.0.html), so you can be feel free to study and contribute.
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
